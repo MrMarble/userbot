050d2cf2d574b0f1ba83724a97f4e136d2356c27
MrMarble <4268580+MrMarble@users.noreply.github.com>
2020-08-20 14:06:53 +0200

    Custom decorator to register modules

---
+++ userbot/decorators.py
@@ -0,0 +1,67 @@
+""" Userbot module for managing events.
+ One of the main components of the userbot. """
+
+from telethon import events
+
+from userbot import bot, LOGS
+
+
+def register(**args):
+    """ Register a new event. """
+    pattern = args.get('pattern', None)
+    disable_edited = args.get('disable_edited', False)
+    ignore_unsafe = args.get('ignore_unsafe', False)
+    unsafe_pattern = r'^[^/!#@\$A-Za-z]'
+    group_only = args.get('group_only', False)
+    insecure = args.get('insecure', False)
+    if pattern is not None and not pattern.startswith('(?i)'):
+        args['pattern'] = '(?i)' + pattern
+
+    if "disable_edited" in args:
+        del args['disable_edited']
+
+    if "ignore_unsafe" in args:
+        del args['ignore_unsafe']
+
+    if "group_only" in args:
+        del args['group_only']
+
+    if "disable_errors" in args:
+        del args['disable_errors']
+
+    if "insecure" in args:
+        del args['insecure']
+
+    if pattern:
+        if not ignore_unsafe:
+            args['pattern'] = args['pattern'].replace('^.', unsafe_pattern, 1)
+
+    def decorator(func):
+        async def wrapper(check):
+            if check.edit_date and check.is_channel and not check.is_group:
+                # Messages sent in channels can be edited by other users.
+                # Ignore edits that take place in channels.
+                return
+            if group_only and not check.is_group:
+                await check.respond("`Are you sure this is a group?`")
+                return
+            if check.via_bot_id and not insecure and check.out:
+                # Ignore outgoing messages via inline bots for security reasons
+                return
+
+            try:
+                await func(check)
+            # This is a gay exception and must be passed out. So that it doesnt spam chats
+            except KeyboardInterrupt:
+                pass
+            except BaseException as e:
+                LOGS.exception(e)  # Log the error in console
+            else:
+                pass
+
+        if not disable_edited:
+            bot.add_event_handler(wrapper, events.MessageEdited(**args))
+        bot.add_event_handler(wrapper, events.NewMessage(**args))
+        return wrapper
+
+    return decorator
