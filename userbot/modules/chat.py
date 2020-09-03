dd4424842fb36e422ded213d53b655ab71eff3c2
MrMarble <4268580+MrMarble@users.noreply.github.com>
2020-09-03 14:27:31 +0200

    Improved Chat!

--- userbot/modules/chat.py
+++ userbot/modules/chat.py
@@ -1,29 +1,32 @@
 """ Userbot module containing userid, chatid commands"""
+from telethon.tl import functions
 
-from userbot import CMD_HELP
+from userbot import CMD_HELP, bot
 from userbot.decorators import register
 
 
-@register(outgoing=True, pattern="^.userid$")
+@register(outgoing=True, pattern="^.userid(?: |$)([\s\S]*)")
 async def useridgetter(target):
     """ For .userid command, returns the ID of the target user. """
-    message = await target.get_reply_message()
-    if message:
-        if not message.forward:
-            user_id = message.sender.id
-            if message.sender.username:
-                name = "@" + message.sender.username
-            else:
-                name = "**" + message.sender.first_name + "**"
 
-        else:
-            user_id = message.forward.sender.id
-            if message.forward.sender.username:
-                name = "@" + message.forward.sender.username
+    username = target.pattern_match.group(1)
+    if username:
+        user = await bot(functions.users.GetFullUserRequest(username))
+        user = user.user
+    else:
+        message = await target.get_reply_message()
+        if message:
+            if not message.forward:
+                user = message.sender
             else:
-                name = "*" + message.forward.sender.first_name + "*"
+                user = message.forward.sender
+    if user:
+        if user.username:
+            name = "@" + user.username
+        else:
+            name = "**" + user.first_name + "**"
         await target.edit("**Name:** {} \n**User ID:** `{}`".format(
-            name, user_id))
+            name, user.id))
 
 
 @register(outgoing=True, pattern="^.chatid$")
@@ -36,5 +39,6 @@ CMD_HELP.update({
     "chat": [
         "Chat", " - `.chatid`: Fetch the current chat's ID.\n"
                 " - `.userid`: Fetch the ID of the user in reply or the original author of a forwarded message.\n"
+                " - `.userid <username>`: Fetch the ID of the provided username.\n"
     ]
 })
